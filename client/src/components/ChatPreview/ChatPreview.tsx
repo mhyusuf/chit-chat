edab72cfa3ccb42b0aead83820f823f408cbdb21
Matt Hurst <matt.adam.hurst@gmail.com>
2020-11-28 12:13:18 +0000

    feat(dom file creation): create pages and components

---
+++ client/src/components/ChatPreview/ChatPreview.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+import './ChatPreview.scss'
+
+const ChatPreview: FunctionComponent = () => {
+  return ( 
+    <div />
+  )
+}
+
+export default ChatPreview;
---
+++ client/src/pages/Rooms/Rooms.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+import './Rooms.scss'
+
+const Rooms: FunctionComponent = () => {
+  return ( 
+    <div />
+  )
+}
+
+export default Rooms;
