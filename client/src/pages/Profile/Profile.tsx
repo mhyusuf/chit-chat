0a2661856f161bf06d6e2e60eff3a483f5a3abb9
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-11-30 10:14:34 +0000

    feat(profile): add teacher profile page

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,8 +1,20 @@
 import React, { FunctionComponent } from "react";
+
 import "./Profile.scss";
 
 const Profile: FunctionComponent<any> = () => {
-  return <div />;
+  return (
+    <div className="profile-grand-wrapper">
+      <div className="profile">
+        <div className="profile__avatar" />
+        <div className="profile__detail">
+          <h1>Sr. Marquez</h1>
+          <p>Mexico City Secondary</p>
+          <button>Log out</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- client/src/pages/TaskDetail/TaskDetail.tsx
+++ client/src/pages/TaskDetail/TaskDetail.tsx
@@ -1,9 +1,4 @@
 import React, { FunctionComponent } from "react";
-import { Link } from "react-router-dom";
-import {
-  IoIosArrowDropdownCircle,
-  IoIosArrowDropupCircle,
-} from "react-icons/io";
 
 import "./TaskDetail.scss";
 
