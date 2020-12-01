699ac6561a1fe8dbc6e0ce9b6c6902347e83426a
bmcglauser <bmcglauser@gmail.com>
2020-12-01 14:52:07 +0000

    feat(server): compelte room controller

--- server/src/controllers/room.controller.js
+++ server/src/controllers/room.controller.js
@@ -1,3 +1,30 @@
-exports.GetRoomsByCourseId = () => {};
+// @ts-nocheck
+const models = require("../models").sequelize.models;
 
-exports.GetRoomByStudentId = () => {};
+exports.GetRoomsByCourseId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const joins = await models.Course_Room.findAll({ where: { CourseId: id } });
+    if (joins) {
+      const rooms = await joins.map((join) =>
+        models.Room.findOne({ where: { id: join.RoomId } })
+      );
+      if (rooms) res.status(200).send(rooms);
+      else res.status(404).send(new Error("No rooms found"));
+    } else res.status(404).send(new Error("No rooms found"));
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
+
+exports.GetRoomByStudentId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await models.Student.findOne({ where: { id } });
+    const room = await models.Room.findOne({ where: { id: student.RoomId } });
+    if (room) res.status(200).send(room);
+    else res.status(404).send(new Error("No room found"));
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
