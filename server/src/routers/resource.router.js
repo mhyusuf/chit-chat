2f9563c3d8f1f1748498212255adc5cb2c6addaf
bmcglauser <bmcglauser@gmail.com>
2020-12-01 15:16:01 +0000

    feat(server): complete resource controller and modify route

--- server/src/routers/resource.router.js
+++ server/src/routers/resource.router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const { GetResources } = require("../controllers/resource.controller");
 
-router.get("/:originalLangauge/:targetLanguage/:level", GetResources);
+router.get("/:nativeLangauge/:targetLanguage/:level", GetResources);
 
 module.exports = router;
