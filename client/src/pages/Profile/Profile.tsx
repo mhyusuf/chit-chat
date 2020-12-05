7dc2d5492091294e51e9c3366b2bb34721cec50f
Matt-Hurst <matt.adam.hurst@gmail.com>
2020-12-05 09:54:26 +0000

    feat(client): give profile page access to user state

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,17 +1,18 @@
 import React, { FunctionComponent } from "react";
 import { connect } from "react-redux";
 import { logout } from "../../actions/index";
+import { User } from "../../interfaces/reducerInterfaces";
 
 import "./Profile.scss";
 
-const Profile: FunctionComponent<any> = ({ history, logout }) => {
+const Profile: FunctionComponent<any> = ({ history, logout, user }) => {
   return (
     <div className="profile-grand-wrapper">
       <div className="profile">
         <div className="profile__avatar" />
         <div className="profile__detail">
-          <h1>Sr. Marquez</h1>
-          <p>Mexico City Secondary</p>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
           <button onClick={() => logout(history)}>Log out</button>
         </div>
       </div>
@@ -19,4 +20,8 @@ const Profile: FunctionComponent<any> = ({ history, logout }) => {
   );
 };
 
-export default connect(null, { logout })(Profile);
+const mapStateToProps = ({ user }: { user: User }) => {
+  return { user };
+};
+
+export default connect(mapStateToProps, { logout })(Profile);
