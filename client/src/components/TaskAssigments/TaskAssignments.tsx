d77365b4f0d978c59f114cefd9133b789b4b980d
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-12-07 10:06:16 +0000

    feat(client): complete assignment-edit form

--- client/src/components/TaskAssigments/TaskAssignments.tsx
+++ client/src/components/TaskAssigments/TaskAssignments.tsx
@@ -1,19 +1,37 @@
 import React, { FunctionComponent } from "react";
+import { Student } from "../../interfaces/reducerInterfaces";
 
 import "./TaskAssignments.scss";
 
-const TaskAssignments: FunctionComponent<any> = ({ students }) => {
-  return students.map((student: any) => (
-    <tr key={student.id}>
-      <td>{student.name}</td>
-      <td>{student.Assignments.length ? "✓" : ""}</td>
-      <td>
-        {student.Assignments.length && student.Assignments[0].fileName
-          ? "✓"
-          : ""}
-      </td>
-    </tr>
-  ));
+interface TaskAssignmentsProps {
+  students: Student[];
+  setShowEdit: (showEdit: boolean) => void;
+}
+
+const TaskAssignments: FunctionComponent<TaskAssignmentsProps> = ({
+  students,
+  setShowEdit,
+}) => {
+  return (
+    <>
+      <div className="task-detail-grand-wrapper__page-content__table-block__student-table">
+        <table>
+          {students.map((student: any) => (
+            <tr key={student.id}>
+              <td>{student.name}</td>
+              <td>{student.Assignments.length ? "✓" : ""}</td>
+              <td>
+                {student.Assignments.length && student.Assignments[0].fileName
+                  ? "✓"
+                  : ""}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <button onClick={() => setShowEdit(true)}>assign</button>
+    </>
+  );
 };
 
 export default TaskAssignments;
