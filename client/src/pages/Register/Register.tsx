f3584113b5c9c28f221941ec7972a827b953998f
Matt-Hurst <matt.adam.hurst@gmail.com>
2020-12-07 17:48:54 +0000

    feat(client): remodel register page add avatar choice

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { registerStudent } from "../../actions/studentActions";
 import { connect } from "react-redux";
 import { User } from "../../interfaces/reducerInterfaces";
+import UserAvatar from "../../components/UserAvatar";
 import "./Register.scss";
-import cat from "../../assets/cat.png";
 
 const Register = ({ registerStudent }: { registerStudent: Function }) => {
   const [input, setInput] = useState({
@@ -17,24 +18,24 @@ const Register = ({ registerStudent }: { registerStudent: Function }) => {
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLFormElement>): void => {
+    console.log(e.target.name, e.target.value);
     setInput((prevInput) => {
       return { ...prevInput, [e.target.name]: e.target.value };
     });
   };
   const handleClick = (e: React.MouseEvent): void => {
     e.preventDefault();
-    console.log(input);
     const { email, name, password, avatar, roomId, courseId } = input;
     registerStudent(email, name, password, avatar, roomId, courseId);
-    // setInput({
-    //   name: "",
-    //   email: "",
-    //   password: "",
-    //   confirmPassword: "",
-    //   courseId: "",
-    //   roomId: "",
-    //   avatar: "cat",
-    // });
+    setInput({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      courseId: "",
+      roomId: "",
+      avatar: "",
+    });
   };
 
   return (
@@ -42,47 +43,86 @@ const Register = ({ registerStudent }: { registerStudent: Function }) => {
       <div className="register-grand-wrapper__form">
         <h2>Register</h2>
         <form action="" onChange={handleChange}>
-          <input
-            type="text"
-            name="name"
-            placeholder="name"
-            value={input.name}
-          />
-          <input
-            type="email"
-            name="email"
-            value={input.email}
-            placeholder="email"
-          />
-          <input
-            type="password"
-            name="password"
-            value={input.password}
-            placeholder="password"
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            value={input.confirmPassword}
-            placeholder="confirm password"
-          />
-          <input
-            type="text"
-            name="courseId"
-            value={input.courseId}
-            id="courseId"
-            placeholder="course id"
-          />
-          <input
-            type="text"
-            name="roomId"
-            value={input.roomId}
-            id="roomId"
-            placeholder="room id"
-          />
+          <div className="register-grand-wrapper__form__div">
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={input.name}
+            />
+            <input
+              type="email"
+              name="email"
+              value={input.email}
+              placeholder="email"
+            />
+          </div>
+          <div className="register-grand-wrapper__form__div">
+            <input
+              type="password"
+              name="password"
+              value={input.password}
+              placeholder="password"
+            />
+            <input
+              type="password"
+              name="confirmPassword"
+              value={input.confirmPassword}
+              placeholder="confirm password"
+            />
+          </div>
+          <div className="register-grand-wrapper__form__div">
+            <div className="register-grand-wrapper__form__div__select">
+              {/* <label htmlFor="avatar-select">Choose an avatar:</label> */}
+              <select name="avatar" id="avatar-select">
+                <option value="">--Choose an avatar:--</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="butterfly">Butterfly</option>
+                <option value="fox">Fox</option>
+                <option value="lion">Lion</option>
+                <option value="frog">Frog</option>
+                <option value="monkey">Monkey</option>
+                <option value="panda">Panda</option>
+                <option value="mouse">Mouse</option>
+              </select>
+            </div>
+            <div
+              style={{
+                width: "20vw",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <UserAvatar
+                avatarString={input.avatar}
+                smaller={true}
+                className="smaller"
+              />
+            </div>
+          </div>
+          <div className="register-grand-wrapper__form__div">
+            <input
+              type="text"
+              name="courseId"
+              value={input.courseId}
+              id="courseId"
+              placeholder="course id"
+            />
+            <input
+              type="text"
+              name="roomId"
+              value={input.roomId}
+              id="roomId"
+              placeholder="room id"
+            />
+          </div>
           <button onClick={handleClick}>Submit</button>
         </form>
       </div>
+      <NavLink to="/">
+        <button className="register-grand-wrapper__back-btn">back</button>
+      </NavLink>
     </div>
   );
 };
@@ -92,11 +132,3 @@ const mapStateToProps = ({ user }: { user: User }) => {
 };
 
 export default connect(mapStateToProps, { registerStudent })(Register);
-
-// avatar: "cat"
-// confirmPassword: "password"
-// courseId: "ffcbd9d6-e39a-4504-83b6-29da127d14bc"
-// email: "andre@andre.com"
-// name: "Andre"
-// password: "password"
-// roomId: "2a38b80c-027e-45c4-b7e5-d431b7179af9"
