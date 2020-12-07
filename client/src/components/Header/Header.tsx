d96802db25efb59adc02e162f9dda39f52a2225d
Matt-Hurst <matt.adam.hurst@gmail.com>
2020-12-07 09:55:05 +0000

    feat(client): add drop down to header with logout func

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { User } from "../../interfaces/reducerInterfaces";
+import { useHistory } from "react-router-dom";
+import { logout } from "../../actions/index";
 import "./Header.scss";
 import UserAvatar from "../UserAvatar";
 
-const Header: FunctionComponent<any> = ({ user }: { user: User }) => {
+// get logout function
+// onClick function to switch local state for avatar
+
+const Header: FunctionComponent<any> = ({ user, logout }) => {
+  const [avatarClicked, setAvatarClicked] = useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>): void => {
+    setAvatarClicked(!avatarClicked);
+  };
+
+  let history = useHistory();
+
   return (
     <div className="header-grand-wrapper">
       <div className="header-grand-wrapper__left-block">
@@ -13,21 +26,40 @@ const Header: FunctionComponent<any> = ({ user }: { user: User }) => {
         <div className="header-grand-wrapper__left-block__logo" />
       </div>
       <div className="header-grand-wrapper__right-block">
-        <NavLink to={user.isTeacher ? "/profile" : "/home"}>
+        <div className="header-grand-wrapper__right-block__user">
           <h3>{user.name}</h3>
-          <UserAvatar
-            avatarString={user.avatar}
-            smaller={true}
-            className="smaller"
-          />
-        </NavLink>
+          <div onClick={handleClick}>
+            <UserAvatar
+              avatarString={user.avatar}
+              smaller={true}
+              className="smaller"
+            />
+          </div>
+        </div>
       </div>
+      {avatarClicked && (
+        <div className="header-grand-wrapper__drop-down-wrapper">
+          {user.isTeacher && (
+            <NavLink to={"/profile"}>
+              <p onClick={handleClick}>profile</p>
+            </NavLink>
+          )}
+          <p
+            onClick={(e) => {
+              handleClick(e);
+              logout(history);
+            }}
+          >
+            logout
+          </p>
+        </div>
+      )}
     </div>
   );
 };
 
-function mapStatetoProps({ user }: { user: User }) {
+function mapStateToProps({ user }: { user: User }) {
   return { user };
 }
 
-export default connect(mapStatetoProps, {})(Header);
+export default connect(mapStateToProps, { logout })(Header);
