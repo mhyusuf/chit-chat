db68b5f83489af961d8ef89acff697a880fd9bdb
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-12-08 16:49:19 +0000

    feat(client): add file-download to assignment-detail

--- server/src/socket.js
+++ server/src/socket.js
@@ -2,7 +2,6 @@ module.exports = (io) => {
   io.on("connect", (socket) => {
     console.log("user connected to io");
     socket.on("message", (data) => {
-      // console.log("emitting", data);
       io.emit("message", data);
     });
   });
